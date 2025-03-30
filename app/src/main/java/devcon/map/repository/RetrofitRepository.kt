29088f59f246f34525e7af174a9806ab74a32093
Todokanai <bh951201@gmail.com>
2025-03-30 16:43:23 +0900

    chore
    주석 수정

--- app/src/main/java/devcon/map/repository/RetrofitRepository.kt
+++ app/src/main/java/devcon/map/repository/RetrofitRepository.kt
@@ -14,7 +14,8 @@ class RetrofitRepository {
         .build()
 
     /** @param query string to query
-     * @param page quantity of page **/
+     * @param page quantity of page
+     * @return result of the query **/
     suspend fun searchKeyword(
         query: String,
         page: Int
@@ -22,9 +23,9 @@ class RetrofitRepository {
         val out = mutableListOf<KeywordDocument>()
         retrofit.create(KeywordSearchService::class.java).run {
             for (i in 1..page) {
-                val response = searchKeyword(query, page = i)      // 각 페이지 마다
+                val response = searchKeyword(query, page = i)
                     .execute()
-                out.addAll(response.body()?.documents ?: emptyList())
+                out.addAll(response.body()?.documents ?: emptyList())   // 쿼리 결과 수집
             }
         }
         return@withContext out
